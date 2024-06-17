25cc59b967545e144bdd6c356bef20ac77b69eaf
Masbe993 <114644735+Masbe993@users.noreply.github.com>
2024-06-18 05:50:46 +1200

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,3 @@
 <?php
 
-    $hostname = "localhost";
-    $userDataBase = "root";
-    $passwordUser = "";
-    $dataBaseName = "pwb";
-
-    $koneksi = mysqli_connect($hostname,$userDataBase,$passwordUser,$dataBaseName) or die (mysqli_error());
-
-    if($koneksi){
-         echo "berhasil koneksi";
-     } else echo "gagal koneksi";
-
-?>
+$db = mysqli_connect("localhost", "root", "", "perpustakaan") or die("gagal konek");
